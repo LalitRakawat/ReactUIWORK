3e2a073e4b4c6fe33024780da617137f5efff030
Lalit Vaishnav <91084455+LalitRakawat@users.noreply.github.com>
2021-10-10 19:43:07 +0530

    Update Menu.jsx

--- antdesignapp/msk/src/pages/Menu.jsx
+++ antdesignapp/msk/src/pages/Menu.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import 'antd/dist/antd.css';
-import { Menu } from 'antd';
+import {Menu} from 'antd';
 
 
+const {SubMenu} = Menu;
 
-const { SubMenu } = Menu;
-
-//Variables ...
+// Variables ...
 let t = [];
 let menuStack = [];
 
@@ -15,82 +14,84 @@ const rootSubmenuKeys = ['101', '107'];
 
 const Sider = () => {
 
-  const [openKeys, setOpenKeys] = React.useState(['101']);
 
-  //Set Menu Key Open-Close
-  const onOpenChange = keys => {
-    const latestOpenKey = keys.find(key => openKeys.indexOf(key) === -1);
-    if (rootSubmenuKeys.indexOf(latestOpenKey) === -1) {
-      setOpenKeys(keys);
-    } else {
-      setOpenKeys(latestOpenKey ? [latestOpenKey] : []);
-    }
-  };
+    const [openKeys, setOpenKeys] = React.useState(['101']);
 
-  //Menu Object
-  let MenuObj = [
-    {   menuId : 101, menuText : "Cloth", parentMenu : 0 ,hasChild : 1},
-    {   menuId : 102, menuText : "Men",   parentMenu : 101 ,hasChild : 1},
-    {   menuId : 103, menuText : "Woman", parentMenu : 101 ,hasChild : 0},
-    {   menuId : 104, menuText : "Child", parentMenu : 101 ,hasChild : 0},
-    {   menuId : 105, menuText : "T-shirt",parentMenu : 102 ,hasChild : 0},
-    {   menuId : 106, menuText : "Gens",parentMenu : 102 ,hasChild : 1},
-    {   menuId : 107, menuText : "Electronics",parentMenu : 0 ,hasChild : 1},
-    {   menuId : 108, menuText : "Keyboard",parentMenu : 107 ,hasChild : 0},
-    {   menuId : 109, menuText : "Mouse",parentMenu : 107 ,hasChild : 0},
-    {   menuId : 110, menuText : "Indian Gens",parentMenu : 106 ,hasChild : 1},
-    {   menuId : 111, menuText : "Non-Indian Gens",parentMenu : 106 ,hasChild : 0},
-    {   menuId : 112, menuText : "SLM",parentMenu : 110 ,hasChild : 0},
-    {   menuId : 113, menuText : "MLS",parentMenu : 110 ,hasChild : 0},
-  ];
+    // Set Menu Key Open-Close
+    const onOpenChange = keys => {
+        const latestOpenKey = keys.find(key => openKeys.indexOf(key) === -1);
+        if (rootSubmenuKeys.indexOf(latestOpenKey) === -1) {
+            setOpenKeys(keys);
+        } else {
+            setOpenKeys(latestOpenKey ? [latestOpenKey] : []);
+        }
+    };
 
-  //Organize The Menu Data In Form Of Parent-Child Form
-  const fnMenuArray = (itm) => {
+    // Menu Object
+    let MenuObj = [
+        {   menuId : 101, menuText : "Cloth", parentMenu : 0 ,hasChild : 1},
+        {   menuId : 102, menuText : "Men",   parentMenu : 101 ,hasChild : 1},
+        {   menuId : 103, menuText : "Woman", parentMenu : 101 ,hasChild : 0},
+        {   menuId : 104, menuText : "Child", parentMenu : 101 ,hasChild : 0},
+        {   menuId : 105, menuText : "T-shirt",parentMenu : 102 ,hasChild : 0},
+        {   menuId : 106, menuText : "Gens",parentMenu : 102 ,hasChild : 1},
+        {   menuId : 107, menuText : "Electronics",parentMenu : 0 ,hasChild : 1},
+        {   menuId : 108, menuText : "Keyboard",parentMenu : 107 ,hasChild : 0},
+        {   menuId : 109, menuText : "Mouse",parentMenu : 107 ,hasChild : 0},
+        {   menuId : 110, menuText : "Indian Gens",parentMenu : 106 ,hasChild : 1},
+        {   menuId : 111, menuText : "Non-Indian Gens",parentMenu : 106 ,hasChild : 0},
+        {   menuId : 112, menuText : "SLM",parentMenu : 110 ,hasChild : 0},
+        {   menuId : 113, menuText : "MLS",parentMenu : 110 ,hasChild : 0},
+      ];
 
-      itm.forEach((o, ind) => {
-        Object.assign((t[o.menuId] = t[o.menuId] || {}), o);
-        t[o.parentMenu] = t[o.parentMenu] || {};
-        t[o.parentMenu].children = t[o.parentMenu].children || [];
-        t[o.parentMenu].children.push(t[o.menuId]);
-      })
+      // Organize The Menu Data In Form Of Parent-Child Form
+    const fnMenuArray = (itm) => {
 
-      menuStack = t[0];
-      t = [];
-  };
+        itm.forEach((o, ind) => {
+            Object.assign((t[o.menuId] = t[o.menuId] || {}), o);
+            t[o.parentMenu] = t[o.parentMenu] || {};
+            t[o.parentMenu].children = t[o.parentMenu].children || [];
+            t[o.parentMenu].children.push(t[o.menuId]);
+        })
 
-  //Function Calling To Organize Menu Data To Create Menu
-  fnMenuArray(MenuObj);
+        menuStack = t[0];
+        t = [];
 
-  //subMenuElement-Componet
-  const subMenuElement = (dataitm) => { 
-    return dataitm.hasChild === 0 
-    ? (    <Menu.Item  key={dataitm.menuId} id={"mnu" + dataitm.menuId}>  {dataitm.menuText} </Menu.Item>  ) 
-    : (
-           <SubMenu key={dataitm.menuId} id={"sub"+ dataitm.menuId} title={dataitm.menuText}>
-              {
-                  dataitm.children ? (dataitm.children.map((itm) => { return <>{subMenuElement(itm)}</>; })) 
-                                  : (<Menu.Item key={dataitm.menuId} id={"mnu" + dataitm.menuId}> {dataitm.menuText} </Menu.Item>)
-              }
-           </SubMenu>
-      );         
-  }
+    };
 
-  //CreateMenu-Componet
-  const CreateMenu = () =>{
-                return (
-                  menuStack.children.map((itm) =>{
-                      if(itm.hasChild){ return subMenuElement(itm); }
-                      else{ return(<Menu.Item key={itm.menuId} id={"mnu" + itm.menuId}>{itm.menuText}</Menu.Item>);}
-                  })
+    // Function Calling To Organize Menu Data To Create Menu
+    fnMenuArray(MenuObj);
+
+    // subMenuElement-Componet
+    const subMenuElement = (dataitm) => {
+        return dataitm.hasChild === 0 
+              ? (  <Menu.Item key={dataitm.menuId}> {dataitm.menuText} </Menu.Item>) 
+              : (  <SubMenu key={ dataitm.menuId } title={dataitm.menuText}>
+                      {
+                        dataitm.children 
+                        ? (dataitm.children.map((itm) => { return <>{ subMenuElement(itm) }</>;})) 
+                        : ( <Menu.Item key={dataitm.menuId}> {dataitm.menuText} </Menu.Item>)
+                      } 
+                    </SubMenu>
                 );
-  }
+    }
 
+    return (
+        <Menu mode="inline" openKeys={openKeys} onOpenChange={onOpenChange} theme="light">
+            {
+                            menuStack.children.map((itm) => {
+                                if (itm.hasChild) {
+                                    return subMenuElement(itm);
+                                } 
+                                else 
+                                {
+                                    return (<Menu.Item key={itm.menuId}>{itm.menuText}</Menu.Item>);
+                                }
+                            })
 
-  return (
-    <Menu mode="inline" openKeys={openKeys} onOpenChange={onOpenChange} style={{ width: 256 ,height: "100vh"}} theme="dark">
-        <CreateMenu/>
-    </Menu>
-  );
+            }
+        </Menu>
+    );
 };
 
 export default Sider
